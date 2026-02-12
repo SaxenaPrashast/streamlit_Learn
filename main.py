1266a5ab53e4dbb12d0d82079def1a1b6e61d3c6
SaxenaPrashast <Saxenaprashast2003@gmail.com>
2026-02-13 01:26:26 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,11 @@
+import streamlit as st
+st.title("Hello World!")
+st.subheader("Welcome to Streamlit")
+st.text("This is a simple web application built with Streamlit.")
+st.write("You can use Streamlit to create interactive web apps easily.")
+
+
+language = st.selectbox("Choose a programming languages:", ["C++", "Java", "Python"])
+
+
+st.write(f"You selected {language}. It's a flavorful blend of spices and tea.") 
